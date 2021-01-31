0f9a6babdea0cc6f4a38b2c7664fbbfb943d9f02
Karina Lukashova <karina-luka@yandex.ru>
2021-01-31 17:55:32 +0300

    Create 2 functions for random number

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно:
+
+const getRandomNumber = function (min, max) {
+  if (min > max) {
+    return 'Значение \'до\' должно быть больше значения \'от\'';
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно:
+const getNumberWithDigits = function (min, max, digits) {
+  if (min > max) {
+    return 'Значение \'до\' должно быть больше значения \'от\'';
+  }
+  return (Math.random() * (max - min) + min).toFixed(digits);
+}
+
+// Примеры:
+getRandomNumber(22,65);
+getNumberWithDigits(22, 54, 2);
