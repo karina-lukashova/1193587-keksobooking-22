f54bbe9d2ccda64a6c61bed50a240c6b0146a6e4
Karina Lukashova <karina-luka@yandex.ru>
2021-02-01 21:10:59 +0300

    Fix functions

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 'use strict'
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно:
+// Фунция для поиска случайного числа в заданном интервале включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5
 const getRandomNumber = (min, max) => {
   if (min > max) {
-    return 'Значение \'до\' должно быть больше значения \'от\'';
+    return 0;
   }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  let result = Math.random() * (max - min + 1) + min;
+  if (result > max) {
+    result = max;
+  }
+  return result;
 }
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно:
-const getNumberWithDigits = (min, max, digits) => {
+// Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
+const getRandomInteger = (min, max) => {
+  return Math.floor(getRandomNumber(min, max));
+}
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+const getCoordinates = (min, max, digits) => {
   if (min > max) {
-    return 'Значение \'до\' должно быть больше значения \'от\'';
+    return '0';
   }
-  return (Math.random() * (max - min) + min).toFixed(digits);
+  return getRandomNumber(min, max).toFixed(digits);
 }
 
 // Примеры:
-getRandomNumber(22,65);
-getNumberWithDigits(22, 54, 2);
+getRandomInteger(20,65);
+getCoordinates(30, 55.547, 3);
