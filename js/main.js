f2054d8ff78d54e86e2dae4328e1bbd870ff3d71
Karina Lukashova <karina-luka@yandex.ru>
2021-02-04 21:32:46 +0300

    Fix getRandomNumber()

--- js/main.js
+++ js/main.js
@@ -6,10 +6,7 @@ const getRandomNumber = (min, max) => {
     return 0;
   }
   let result = Math.random() * (max - min + 1) + min;
-  if (result > max) {
-    result = max;
-  }
-  return result;
+  return result > max ? max : result;
 }
 
 // Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
