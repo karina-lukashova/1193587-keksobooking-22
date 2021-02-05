de462083430a659c9754c03e63abcc7ea1428ad0
Karina Lukashova <karina-luka@yandex.ru>
2021-02-05 23:38:57 +0300

    Add var and functions for stays array creation

--- js/main.js
+++ js/main.js
@@ -22,6 +22,131 @@ const getCoordinates = (min, max, digits) => {
   return getRandomNumber(min, max).toFixed(digits);
 }
 
-// Примеры:
-getRandomInteger(20,65);
-getCoordinates(30, 55.547, 3);
+// Создание массива из объявлений:
+
+// Переменные-константы:
+const TITLES = [
+  'Студия в традиционном стиле',
+  'Двухкомнатная квартира со всеми удобствами',
+  'Квартира с прекрасным видом на Tokyo Skytree',
+  'Жильё в историческом центре',
+  'Современные аппартаменты в районе Гиндза',
+  'Скромная студия с самым необходимым',
+  'Королевский дворец в пригороде',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECKIN_OUT_HOURS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const ALL_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+]
+
+const ALL_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+]
+
+const DESCRIPTIONS = [
+  'Небольшая студия в истинно японском стиле погрузит вас в атмосферу страны восходящего солнца. Почувствуй себя коренным жителем!',
+  'Стандартная квартира для двоих человек с современной техникой в безопасном районе Токио.',
+  'Из наших панорамных окон вы увидите всю красоту города, а также великолепную Tokyo Skytree, самую высокую телебашню мира',
+  'Поселившись у нас в самом центре города, вы пешком сможете обойти главные достопримечательности Токио',
+  'Добро пожаловать в один из самых роскошных районов Токио! В шаговой доступности от квартиры лучшие рестораны, бары и культурные развлечения',
+  'За небольшую цену вы получите полноценную студию с собственной ванной и мини-кухней',
+  'Почувствуйте себя королём в нашем дворце! В вашем распоряжении личный онсен, кинозал и прекрасная веранда с видом на собственный парк',
+];
+
+const SIMILAR_STAYS_COUNT = 10;
+
+// Переменные и функции расчета:
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+// Сначала попробовала сделать уникальный массив через Set, но этот способ оказался в 6 раз медленнее, чем способ через Array.some (проверила на массиве из 5000 элементов). Но на всякий случай оставила код с Set (ниже на примере features) - насколько он в целом выглядит адекватно?)
+// let features = [];
+// while (features.length !== 5000) {
+//   features.splice(features.length - 1, 0, getRandomArrayElement(ALL_FEATURES));
+//   const featuresSet = new Set(features);
+//   features = Array.from(featuresSet);
+// }
+
+// А вот это рабочий:
+const getUniqueArray = function (allArr, count) {
+  const newArr = [];
+  while (newArr.length !== count) {
+    const item = getRandomArrayElement(allArr);
+    if (!newArr.some(value => item === value)) {
+      newArr.splice(0, 0, item);
+    }
+  }
+  return newArr;
+}
+
+// Функции для получения объектов объявлений:
+
+const getAuthor = function () {
+  const author = {
+    avatar: `img/avatars/user0${getRandomInteger(1, 8)}.png`,
+  };
+  return author;
+}
+
+let coordinates = {};
+const getLocation = function () {
+  const location = {
+    x: getCoordinates(35.65000, 35.70000, 5),
+    y: getCoordinates(139.70000, 139.80000, 5),
+  };
+  coordinates = location;
+  return location;
+}
+
+const getOffer = function () {
+  const featuresCount = getRandomInteger(0, ALL_FEATURES.length); // От нуля, т.к. предполагаю, что есть жилье без этих доп. удобств
+  const photosCount = getRandomInteger(1, ALL_PHOTOS.length);
+
+  const offer = {
+    title: getRandomArrayElement(TITLES),
+    address: `${coordinates.x}, ${coordinates.y}`,
+    price: getRandomInteger(0, 1000000), // В ТЗ максимальное значение = 1 000 000, поэтому поставила его как максимальное. Включила 0, т.к. в ТЗ минимальная цена на "Бунгало" - 0 рублей
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomInteger(1, 100), // Подумала, что вряд ли будет жилье больше 100 комнат :) Плюс, в ТЗ 100 комнат значится уже как "не для гостей" - приняла это за максимум
+    guests: getRandomInteger(0, 100), // По ТЗ прослеживается связь: 1 комната = 1 человек. Поэтому временно поставила здесь максимум такой же, как для rooms. От нуля, т.к. при 100 комнатах по условию - не для гостей, значит их 0
+    checkin: getRandomArrayElement(CHECKIN_OUT_HOURS),
+    checkout: getRandomArrayElement(CHECKIN_OUT_HOURS),
+    features: getUniqueArray(ALL_FEATURES, featuresCount),
+    description: getRandomArrayElement(DESCRIPTIONS),
+    photos: getUniqueArray(ALL_PHOTOS, photosCount),
+  };
+  return offer;
+}
+
+// Создаём объявление
+const createStay = () => {
+  return {
+    author: getAuthor(),
+    location: getLocation(), // Поставила location перед offer, т.к. по условию задачи адрес в offer должен ссылаться на location
+    offer: getOffer(),
+  }
+};
+
+const similarStays = new Array(SIMILAR_STAYS_COUNT).fill(null).map(() => createStay());
+similarStays; //Написала это, чтобы eslint не ругался на similarStays, которую мы создали, но не использовали
