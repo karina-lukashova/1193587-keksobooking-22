98264c6c9bd44a3f91f782a1588e32039ccebb70
Karina Lukashova <karina-luka@yandex.ru>
2021-02-09 00:18:22 +0300

    Fix getSimilarStays function and other related

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 'use strict'
 
-// Фунция для поиска случайного числа в заданном интервале включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5
-const getRandomNumber = (min, max) => {
-  if (min > max) {
-    return 0;
-  }
-  let result = Math.random() * (max - min + 1) + min;
-  return result > max ? max : result;
-}
-
-// Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
-const getRandomInteger = (min, max) => {
-  return Math.floor(getRandomNumber(min, max));
-}
-
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-const getCoordinates = (min, max, digits) => {
-  if (min > max) {
-    return '0';
-  }
-  return getRandomNumber(min, max).toFixed(digits);
-}
-
-// Создание массива из объявлений:
-
 // Переменные-константы:
 const TITLES = [
   'Студия в традиционном стиле',
@@ -48,7 +24,7 @@ const CHECKIN_OUT_HOURS = [
   '14:00',
 ];
 
-const ALL_FEATURES = [
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -57,7 +33,7 @@ const ALL_FEATURES = [
   'conditioner',
 ]
 
-const ALL_PHOTOS = [
+const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
@@ -73,80 +49,100 @@ const DESCRIPTIONS = [
   'Почувствуйте себя королём в нашем дворце! В вашем распоряжении личный онсен, кинозал и прекрасная веранда с видом на собственный парк',
 ];
 
+const AVATARS_COUNT = 8;
+const MAX_PRICE = 1000000;
+const MAX_ROOMS_COUNT = 100;
+const MAX_GUESTS_COUNT = 100;
+const MIN_X_COORDINATE = 35.65000;
+const MAX_X_COORDINATE = 35.70000;
+const MIN_Y_COORDINATE = 139.70000;
+const MAX_Y_COORDINATE = 139.80000;
+const COORDINATE_DECIMALS = 5;
+
 const SIMILAR_STAYS_COUNT = 10;
 
-// Переменные и функции расчета:
+
+// Фунция для поиска случайного числа в заданном интервале включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5
+const getRandomNumber = (min, max) => {
+  if (min > max) {
+    return 0;
+  }
+  let result = Math.random() * (max - min + 1) + min;
+  return result > max ? max : result;
+}
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
+const getRandomInteger = (min, max) => {
+  return Math.floor(getRandomNumber(min, max));
+}
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+const getCoordinates = (min, max, digits) => {
+  if (min > max) {
+    return '0';
+  }
+  return getRandomNumber(min, max).toFixed(digits);
+}
+
+// Получаем случайный элемент массива:
 const getRandomArrayElement = (elements) => {
   return elements[getRandomInteger(0, elements.length - 1)];
 };
 
-// Сначала попробовала сделать уникальный массив через Set, но этот способ оказался в 6 раз медленнее, чем способ через Array.some (проверила на массиве из 5000 элементов). Но на всякий случай оставила код с Set (ниже на примере features) - насколько он в целом выглядит адекватно?)
-// let features = [];
-// while (features.length !== 5000) {
-//   features.splice(features.length - 1, 0, getRandomArrayElement(ALL_FEATURES));
-//   const featuresSet = new Set(features);
-//   features = Array.from(featuresSet);
-// }
-
-// А вот это рабочий:
-const getUniqueArray = function (allArr, count) {
+// Получаем уникальный массив из count элементов:
+const getUniqueArray = (allArr, count) => {
   const newArr = [];
-  while (newArr.length !== count) {
-    const item = getRandomArrayElement(allArr);
-    if (!newArr.some(value => item === value)) {
-      newArr.splice(0, 0, item);
-    }
+  for (let i = 1; i <= count; i++) {
+    newArr.push(getRandomArrayElement(allArr));
   }
-  return newArr;
+  const uniqueSet = new Set(newArr);
+  return Array.from(uniqueSet);
 }
 
-// Функции для получения объектов объявлений:
-
-const getAuthor = function () {
-  const author = {
-    avatar: `img/avatars/user0${getRandomInteger(1, 8)}.png`,
+// Получаем случайные координаты из заданного интервала
+const getLocation = () => {
+  return {
+    x: getCoordinates(MIN_X_COORDINATE, MAX_X_COORDINATE, COORDINATE_DECIMALS),
+    y: getCoordinates(MIN_Y_COORDINATE, MAX_Y_COORDINATE, COORDINATE_DECIMALS),
   };
-  return author;
-}
-
-let coordinates = {};
-const getLocation = function () {
-  const location = {
-    x: getCoordinates(35.65000, 35.70000, 5),
-    y: getCoordinates(139.70000, 139.80000, 5),
-  };
-  coordinates = location;
-  return location;
-}
-
-const getOffer = function () {
-  const featuresCount = getRandomInteger(0, ALL_FEATURES.length); // От нуля, т.к. предполагаю, что есть жилье без этих доп. удобств
-  const photosCount = getRandomInteger(1, ALL_PHOTOS.length);
-
-  const offer = {
-    title: getRandomArrayElement(TITLES),
-    address: `${coordinates.x}, ${coordinates.y}`,
-    price: getRandomInteger(0, 1000000), // В ТЗ максимальное значение = 1 000 000, поэтому поставила его как максимальное. Включила 0, т.к. в ТЗ минимальная цена на "Бунгало" - 0 рублей
-    type: getRandomArrayElement(TYPES),
-    rooms: getRandomInteger(1, 100), // Подумала, что вряд ли будет жилье больше 100 комнат :) Плюс, в ТЗ 100 комнат значится уже как "не для гостей" - приняла это за максимум
-    guests: getRandomInteger(0, 100), // По ТЗ прослеживается связь: 1 комната = 1 человек. Поэтому временно поставила здесь максимум такой же, как для rooms. От нуля, т.к. при 100 комнатах по условию - не для гостей, значит их 0
-    checkin: getRandomArrayElement(CHECKIN_OUT_HOURS),
-    checkout: getRandomArrayElement(CHECKIN_OUT_HOURS),
-    features: getUniqueArray(ALL_FEATURES, featuresCount),
-    description: getRandomArrayElement(DESCRIPTIONS),
-    photos: getUniqueArray(ALL_PHOTOS, photosCount),
-  };
-  return offer;
 }
 
 // Создаём объявление
 const createStay = () => {
+  const featuresCount = getRandomInteger(0, FEATURES.length); // От нуля, т.к. предполагаю, что есть жилье без этих доп. удобств
+  const photosCount = getRandomInteger(1, PHOTOS.length);
+  const coordinates = getLocation();
+
   return {
-    author: getAuthor(),
-    location: getLocation(), // Поставила location перед offer, т.к. по условию задачи адрес в offer должен ссылаться на location
-    offer: getOffer(),
+    author: {
+      avatar: `img/avatars/user0${getRandomInteger(1, AVATARS_COUNT)}.png`,
+    },
+
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${coordinates.x}, ${coordinates.y}`,
+      price: getRandomInteger(0, MAX_PRICE),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInteger(1, MAX_ROOMS_COUNT),
+      guests: getRandomInteger(0, MAX_GUESTS_COUNT),
+      checkin: getRandomArrayElement(CHECKIN_OUT_HOURS),
+      checkout: getRandomArrayElement(CHECKIN_OUT_HOURS),
+      features: getUniqueArray(FEATURES, featuresCount),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getUniqueArray(PHOTOS, photosCount),
+    },
+
+    location: coordinates,
   }
 };
 
-const similarStays = new Array(SIMILAR_STAYS_COUNT).fill(null).map(() => createStay());
-similarStays; //Написала это, чтобы eslint не ругался на similarStays, которую мы создали, но не использовали
+// Создаём массив объявлений
+const getSimilarStays = (count) => {
+  const similarStays = [];
+  for (let i = 1; i <= count; i++) {
+    similarStays.push(createStay());
+  }
+  return similarStays;
+};
+
+getSimilarStays(SIMILAR_STAYS_COUNT); //Написала это, чтобы eslint не ругался на similarStays, которую мы создали, но не использовали
