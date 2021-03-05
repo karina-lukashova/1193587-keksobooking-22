edce6802d77e76ea9703d97e0b33532183927096
Karina Lukashova <karina-luka@yandex.ru>
2021-03-05 13:34:44 +0300

    Fix validation functions

--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@ import {mainMarker} from './map.js';
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
 const MIN_PRICE = 0;
+const MAX_CAPACITY = '100';
 const MAX_PRICE = 1000000;
 const DIGITS_NUMBER = 5;
 
@@ -15,13 +16,6 @@ const priceElement = document.querySelector('#price');
 const roomNumberElement = document.querySelector('#room_number');
 const capacityElement = document.querySelector('#capacity');
 
-const roomCapacityMap = {
-  '1': ['1'],
-  '2': ['1', '2'],
-  '3': ['1', '2', '3'],
-  '100': ['0'],
-}
-
 const minPriceForTypeMap = {
   'bungalow': 0,
   'flat': 1000,
@@ -29,19 +23,42 @@ const minPriceForTypeMap = {
   'palace': 10000,
 }
 
-// Функция проверки равенства элемента массива значению из capacityElement:
-const hasCapacityValue = (element) => {
-  return element === capacityElement.value;
+// Функция валидации текста на нужное количество символов:
+const onTextValidate = ({currentTarget}) => {
+  const titleLength = currentTarget.value.length;
+  if (titleLength < MIN_TITLE_LENGTH) {
+    currentTarget.setCustomValidity('Введите ещё ' + (MIN_TITLE_LENGTH - titleLength) +' симв.');
+  } else if (titleLength > MAX_TITLE_LENGTH) {
+    currentTarget.setCustomValidity('Удалите лишние ' + (titleLength - MAX_TITLE_LENGTH) +' симв.');
+  } else {
+    currentTarget.setCustomValidity('');
+  }
+
+  currentTarget.reportValidity();
+}
+
+// Функция валидации цены на минимум и максимум:
+const onPriceValidate = ({currentTarget}) => {
+  const priceValue = currentTarget.value;
+  if (priceValue < MIN_PRICE) {
+    currentTarget.setCustomValidity('Цена должна быть минимум 0 рублей');
+  } else if (priceValue > MAX_PRICE) {
+    currentTarget.setCustomValidity('Цена должна быть максимум 1 000 000 рублей');
+  } else {
+    currentTarget.setCustomValidity('');
+  }
+
+  currentTarget.reportValidity();
 }
 
 // Функция валидации количества комнат и гостей:
-const validateRoomCapacity = () => {
-  if (!roomCapacityMap[roomNumberElement.value].some(hasCapacityValue)) {
-    if (roomNumberElement.value === '100') {
-      capacityElement.setCustomValidity('100 комнат может быть только "не для гостей"');
-    } else {
-      capacityElement.setCustomValidity(`Для выбранного количества комнат количество гостей должно быть: ${roomCapacityMap[roomNumberElement.value]}`);
-    }
+const validateRoomCapacity = (value) => {
+  if (value === MAX_CAPACITY && capacityElement.value!=='0') {
+    capacityElement.setCustomValidity('100 комнат может быть только "не для гостей"');
+  } else if (value!==MAX_CAPACITY && capacityElement.value=='0') {
+    capacityElement.setCustomValidity('Выберите количество гостей, оно не может равняться 0');
+  } else if (value < capacityElement.value) {
+    capacityElement.setCustomValidity(`Количество гостей должно быть не больше ${value}`);
   } else {
     capacityElement.setCustomValidity('');
   }
@@ -61,38 +78,16 @@ const synchronizeTime = (firstTime, secondTime) => {
 }
 
 // Валидация названия объявления:
-titleElement.addEventListener('input', () => {
-  const titleLength = titleElement.value.length;
-  if (titleLength < MIN_TITLE_LENGTH) {
-    titleElement.setCustomValidity('Введите ещё ' + (MIN_TITLE_LENGTH - titleLength) +' симв.');
-  } else if (titleLength > MAX_TITLE_LENGTH) {
-    titleElement.setCustomValidity('Удалите лишние ' + (titleLength - MAX_TITLE_LENGTH) +' симв.');
-  } else {
-    titleElement.setCustomValidity('');
-  }
-
-  titleElement.reportValidity();
-})
+titleElement.addEventListener('input', onTextValidate);
 
 // Валидация цены:
-priceElement.addEventListener('input', () => {
-  const priceValue = priceElement.value;
-  if (priceValue < MIN_PRICE) {
-    priceElement.setCustomValidity('Цена должна быть минимум 0 рублей');
-  } else if (priceValue > MAX_PRICE) {
-    priceElement.setCustomValidity('Цена должна быть максимум 1 000 000 рублей');
-  } else {
-    priceElement.setCustomValidity('');
-  }
-
-  priceElement.reportValidity();
-})
+priceElement.addEventListener('input', onPriceValidate);
 
 // Валидация соответствия количества гостей от количества комнат - при изменении количества комнат:
-roomNumberElement.addEventListener('input', () => validateRoomCapacity())
+roomNumberElement.addEventListener('input', () => validateRoomCapacity(roomNumberElement.value))
 
 // Валидация соответствия количества гостей от количества комнат - при изменении количества гостей:
-capacityElement.addEventListener('input', () => validateRoomCapacity())
+capacityElement.addEventListener('input', () => validateRoomCapacity(roomNumberElement.value))
 
 // Синхронизация цены и типа жилья
 stayTypeElement.addEventListener('input', () => {
